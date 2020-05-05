a7d34f6c5194a65b3dd21f3c7574aba3950aaf30
Tsuyoshi Inoue <inoue0406@gmail.com>
2020-05-05 11:34:10 +0900

    added trajGRU code

--- run/run_20200503_LTM_size128_step6_ver4.bash
+++ run/run_20200503_LTM_size128_step6_ver4.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-case="result_20200503_LTM_size128_st6_ver4"
-
-# trial script for local transformation network
-python ../src/main_LTM_jma.py --data_path ../data/data_kanto_resize/ \
-       --valid_data_path ../data/data_kanto_resize/ \
-       --train_path ../data/train_simple_JMARadar.csv \
-       --valid_path ../data/valid_simple_JMARadar.csv \
-       --test --eval_threshold 0.5 10 20 --test_path ../data/valid_simple_JMARadar.csv \
-       --result_path $case --tdim_use 12 --learning_rate 0.01 --lr_decay 0.95 --steps_in_itr 5 \
-       --batch_size 60 --n_epochs 50 --n_threads 4 --checkpoint 10 \
-       --optimizer adam --model_name ltm --in_channels 2 --loss_used_step 6
-# png output
-python ../src_post/plot_comp_prediction_ltm.py $case
+#case="result_20200503_LTM_size128_st6_ver4"
+#
+## trial script for local transformation network
+#python ../src/main_LTM_jma.py --data_path ../data/data_kanto_resize/ \
+#       --valid_data_path ../data/data_kanto_resize/ \
+#       --train_path ../data/train_simple_JMARadar.csv \
+#       --valid_path ../data/valid_simple_JMARadar.csv \
+#       --test --eval_threshold 0.5 10 20 --test_path ../data/valid_simple_JMARadar.csv \
+#       --result_path $case --tdim_use 12 --learning_rate 0.01 --lr_decay 0.95 --steps_in_itr 5 \
+#       --batch_size 60 --n_epochs 50 --n_threads 4 --checkpoint 10 \
+#       --optimizer adam --model_name ltm --in_channels 2 --loss_used_step 6
+## png output
+#python ../src_post/plot_comp_prediction_ltm.py $case
 
 case="result_20200503_LTM_size128_stair_ver4"
 
@@ -22,9 +22,9 @@ python ../src/main_LTM_jma.py --data_path ../data/data_kanto_resize/ \
        --train_path ../data/train_simple_JMARadar.csv \
        --valid_path ../data/valid_simple_JMARadar.csv \
        --test --eval_threshold 0.5 10 20 --test_path ../data/valid_simple_JMARadar.csv \
-       --result_path $case --tdim_use 12 --learning_rate 0.01 --lr_decay 0.95 --steps_in_itr 5 \
+       --result_path $case --tdim_use 12 --learning_rate 0.01 --lr_decay 0.95 --steps_in_itr 1 \
        --batch_size 60 --n_epochs 50 --n_threads 4 --checkpoint 10 \
-       --optimizer adam --model_name ltm --in_channels 2 --loss_used_step 1 2 3 4 5 6
+       --optimizer adam --model_name ltm --in_channels 3 --loss_used_step 1 2 3 4 5 6
 # png output
 python ../src_post/plot_comp_prediction_ltm.py $case
 
---
+++ src/main_trajGRU_jma.py
@@ -0,0 +1,241 @@
+import numpy as np
+import torch 
+import torchvision
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+import pandas as pd
+import h5py
+import os
+import sys
+import json
+import time
+
+import pdb
+
+from jma_pytorch_dataset import *
+from scaler import *
+from train_valid_epoch import *
+from utils import Logger
+from opts import parse_opts
+from loss_funcs import *
+
+
+# trajGRU model
+from collections import OrderedDict
+from models_trajGRU.forecaster import Forecaster
+from models_trajGRU.encoder import Encoder
+from models_trajGRU.model import EF
+from models_trajGRU.trajGRU import TrajGRU
+from models_trajGRU.convLSTM import ConvLSTM
+device = torch.device("cuda")
+
+
+def count_parameters(model,f):
+    for name,p in model.named_parameters():
+        f.write("name,"+name+", Trainable, "+str(p.requires_grad)+",#params, "+str(p.numel())+"\n")
+    Nparam = sum(p.numel() for p in model.parameters())
+    Ntrain = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    f.write("Number of params:"+str(Nparam)+", Trainable parameters:"+str(Ntrain)+"\n")
+    
+if __name__ == '__main__':
+   
+    # parse command-line options
+    opt = parse_opts()
+    print(opt)
+    # create result dir
+    if not os.path.exists(opt.result_path):
+        os.mkdir(opt.result_path)
+    
+    with open(os.path.join(opt.result_path, 'opts.json'), 'w') as opt_file:
+        json.dump(vars(opt), opt_file)
+
+    # generic log file
+    logfile = open(os.path.join(opt.result_path, 'log_run.txt'),'w')
+    logfile.write('Start time:'+time.ctime()+'\n')
+    tstart = time.time()
+
+    # model information
+    modelinfo = open(os.path.join(opt.result_path, 'model_info.txt'),'w')
+
+
+    # model structure
+    # build model
+    # modified for 128x128
+    convlstm_encoder_params = [
+        [
+            OrderedDict({'conv1_leaky_1': [1, 8, 7, 3, 1]}),
+            OrderedDict({'conv2_leaky_1': [64, 192, 5, 3, 1]}),
+            OrderedDict({'conv3_leaky_1': [192, 192, 3, 2, 1]}),
+        ],
+
+        [
+            ConvLSTM(input_channel=8, num_filter=64, b_h_w=(opt.batch_size, 42, 42),
+                     kernel_size=3, stride=1, padding=1),
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(opt.batch_size, 14, 14),
+                     kernel_size=3, stride=1, padding=1),
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(opt.batch_size, 7, 7),
+                     kernel_size=3, stride=1, padding=1),
+        ]
+    ]
+
+    convlstm_forecaster_params = [
+        [
+            OrderedDict({'deconv1_leaky_1': [192, 192, 4, 2, 1]}),
+            OrderedDict({'deconv2_leaky_1': [192, 64, 5, 3, 1]}),
+            OrderedDict({
+                'deconv3_leaky_1': [64, 8, 7, 3, 1],
+                'conv3_leaky_2': [8, 8, 3, 1, 1],
+                'conv3_3': [8, 1, 1, 1, 0]
+            }),
+        ],
+
+        [
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(opt.batch_size, 7, 7),
+                     kernel_size=3, stride=1, padding=1),
+            ConvLSTM(input_channel=192, num_filter=192, b_h_w=(opt.batch_size, 14, 14),
+                     kernel_size=3, stride=1, padding=1),
+            ConvLSTM(input_channel=64, num_filter=64, b_h_w=(opt.batch_size, 42, 42),
+                     kernel_size=3, stride=1, padding=1),
+        ]
+    ]
+
+
+    # prepare scaler for data
+    if opt.data_scaling == 'linear':
+        scl = LinearScaler()
+    if opt.data_scaling == 'root':
+        scl = RootScaler()
+    if opt.data_scaling == 'root_int':
+        scl = RootIntScaler()
+    elif opt.data_scaling == 'log':
+        scl = LogScaler()
+        
+    if not opt.no_train:
+        # loading datasets
+        train_dataset = JMARadarDataset(root_dir=opt.data_path,
+                                        csv_file=opt.train_path,
+                                        tdim_use=opt.tdim_use,
+                                        transform=None)
+    
+        valid_dataset = JMARadarDataset(root_dir=opt.valid_data_path,
+                                        csv_file=opt.valid_path,
+                                        tdim_use=opt.tdim_use,
+                                        transform=None)
+    
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                   batch_size=opt.batch_size,
+                                                   num_workers=4,
+                                                   drop_last=True,
+                                                   shuffle=True)
+    
+        valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset,
+                                                   batch_size=opt.batch_size,
+                                                   num_workers=4,
+                                                   drop_last=True,
+                                                   shuffle=False)
+
+        if opt.model_name == 'clstm':
+            # convolutional lstm
+            encoder = Encoder(convlstm_encoder_params[0], convlstm_encoder_params[1]).to(device)
+            forecaster = Forecaster(convlstm_forecaster_params[0], convlstm_forecaster_params[1]).to(device)
+            convlstm = EF(encoder, forecaster).to(device)
+        elif opt.model_name == 'clstm_upper':
+            pass
+    
+        if opt.transfer_path != 'None':
+            # Use pretrained weights for transfer learning
+            print('loading pretrained model:',opt.transfer_path)
+            convlstm = torch.load(opt.transfer_path)
+
+        modelinfo.write('Model Structure \n')
+        modelinfo.write(str(convlstm))
+        count_parameters(convlstm,modelinfo)
+        modelinfo.close()
+        
+        if opt.loss_function == 'MSE':
+            loss_fn = torch.nn.MSELoss()
+        elif opt.loss_function == 'WeightedMSE':
+            loss_fn = weighted_MSE_loss
+        elif opt.loss_function == 'MaxMSE':
+            loss_fn = max_MSE_loss(opt.loss_weights)
+        elif opt.loss_function == 'MultiMSE':
+            loss_fn = multi_MSE_loss(opt.loss_weights)
+
+        # Type of optimizers adam/rmsprop
+        if opt.optimizer == 'adam':
+            optimizer = torch.optim.Adam(convlstm.parameters(), lr=opt.learning_rate)
+        elif opt.optimizer == 'rmsprop':
+            optimizer = torch.optim.RMSprop(convlstm.parameters(), lr=opt.learning_rate)
+            
+        # learning rate scheduler
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=opt.lr_decay)
+            
+        # Prep logger
+        train_logger = Logger(
+            os.path.join(opt.result_path, 'train.log'),
+            ['epoch', 'loss', 'lr'])
+        train_batch_logger = Logger(
+            os.path.join(opt.result_path, 'train_batch.log'),
+            ['epoch', 'batch', 'loss', 'lr'])
+        valid_logger = Logger(
+            os.path.join(opt.result_path, 'valid.log'),
+            ['epoch', 'loss'])
+    
+        # training 
+        for epoch in range(1,opt.n_epochs+1):
+            # step scheduler
+            scheduler.step()
+            # training & validation
+            train_epoch(epoch,opt.n_epochs,train_loader,convlstm,loss_fn,optimizer,
+                        train_logger,train_batch_logger,opt,scl)
+            #valid_epoch(epoch,opt.n_epochs,valid_loader,convlstm,loss_fn,
+            #            valid_logger,opt,scl)
+
+            if epoch % opt.checkpoint == 0:
+                # save the trained model for every checkpoint
+                # (1) as binary 
+                torch.save(convlstm,os.path.join(opt.result_path,
+                                                 'trained_CLSTM_epoch%03d.model' % epoch))
+                # (2) as state dictionary
+                torch.save(convlstm.state_dict(),
+                           os.path.join(opt.result_path,
+                                        'trained_CLSTM_epoch%03d.dict' % epoch))
+        # save the trained model
+        # (1) as binary 
+        torch.save(convlstm,os.path.join(opt.result_path, 'trained_CLSTM.model'))
+        # (2) as state dictionary
+        torch.save(convlstm.state_dict(),
+                   os.path.join(opt.result_path, 'trained_CLSTM.dict'))
+
+    # test datasets if specified
+    if opt.test:
+        if opt.no_train:
+            #load pretrained model from results directory
+            model_fname = os.path.join(opt.result_path, 'trained_CLSTM.model')
+            print('loading pretrained model:',model_fname)
+            convlstm = torch.load(model_fname)
+            loss_fn = torch.nn.MSELoss()
+            
+        # prepare loader
+        test_dataset = JMARadarDataset(root_dir=opt.valid_data_path,
+                                        csv_file=opt.test_path,
+                                        tdim_use=opt.tdim_use,
+                                        transform=None)
+        test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                                   batch_size=opt.batch_size,
+                                                   num_workers=4,
+                                                   drop_last=True,
+                                                   shuffle=False)
+
+        
+        
+        # testing for the trained model
+        for threshold in opt.eval_threshold:
+            test_CLSTM_EP(test_loader,convlstm,loss_fn,opt,scl,threshold)
+
+    # output elapsed time
+    logfile.write('End time: '+time.ctime()+'\n')
+    tend = time.time()
+    tdiff = float(tend-tstart)/3600.0
+    logfile.write('Elapsed time[hours]: %f \n' % tdiff)
