0bd6400793617f0d4c5e996aaf24ee862ed497d8
Tsuyoshi Inoue <inoue0406@gmail.com>
2020-05-17 12:22:09 +0900

    starting to add rev_trilinear

--- run/run_20200512_euler-lagrange-const_v2.bash
+++ run/run_20200512_euler-lagrange-const_v2.bash
@@ -10,3 +10,6 @@ python ../src/main_Euler-Lagrange_jma.py --data_path ../data/data_kanto_resize/ 
        --result_path $case --model_name euler-lagrange-const --tdim_use 12 --learning_rate 0.005 --lr_decay 0.9 \
        --batch_size 10 --n_epochs 20 --n_threads 4 --checkpoint 10 \
        --hidden_channels 8 --kernel_size 3 --optimizer adam
+# post plot
+python ../src_post/plot_comp_prediction_euler-lagrange.py $case
+python ../src_post/gif_animation.py $case
