5c98cb4564c27e4a5f5214876ea57b65e81ccefb
Tsuyoshi Inoue <inoue0406@gmail.com>
2020-05-19 22:23:03 +0900

    small tweaks

--- src/rev_trilinear/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-
-setup(name='rev_trilinear',
-      ext_modules=[CUDAExtension('rev_trilinear', ['rev_trilinear.cpp', 'forward.cu', 'backward.cu'])],
-      cmdclass={'build_ext': BuildExtension})
