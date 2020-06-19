fixed incosistency in Euler-Lagrange calculation

src/artfield_pytorch_dataset.py
@@ -34,6 +34,7 @@ class ArtfieldDataset(data.Dataset):
         return len(self.df_fnames)
         
     def __getitem__(self, index):
+        fnames = self.df_fnames.iloc[index].loc['fname']
         # read X
         h5_name_all = os.path.join(self.root_dir, self.df_fnames.iloc[index].loc['fname'])
         h5file = h5py.File(h5_name_all,'r')
@@ -47,16 +48,18 @@ class ArtfieldDataset(data.Dataset):
             vel = vel[None,:,:,:] # add "time" dimension as 1
             # broadcast along time axis
             _,_,H,W = vel.shape
-            rain_Y = np.zeros((self.tdim_use,2,H,W))
-            rain_Y[:,:,:,:] = vel 
+            UV = np.zeros((self.tdim_use,2,H,W))
+            UV[:,:,:,:] = vel
+            rain_Y = rain_all[self.tdim_use:(self.tdim_use*2),:,:,:] # use time tdim_use as X 
+            sample = {'past': rain_X, 'future': UV, 'future_val':rain_Y,
+                      'fnames':fnames}
         elif self.mode == "run":
             rain_Y = rain_all[self.tdim_use:(self.tdim_use*2),:,:,:] # use time tdim_use as X 
+            sample = {'past': rain_X, 'future': rain_Y,
+                      'fnames':fnames}
         h5file.close()
         
         # save
-        fnames = self.df_fnames.iloc[index].loc['fname']
-        sample = {'past': rain_X, 'future': rain_Y,
-                  'fnames':fnames}
 
         if self.transform:
             sample = self.transform(sample)

src/models_trajGRU/model_euler_lagrange.py
@@ -64,6 +64,7 @@ def pc_to_grid(R_pc,XY_pc,height):
     
     # apply interpolation
     Rgrd = RevBilinear.apply(XY_pc, R_pc, height)
+    Rgrd = Rgrd.permute(0,1,3,2)
     return Rgrd
 
 class EF_el(nn.Module):
@@ -95,6 +96,10 @@ class EF_el(nn.Module):
             print('max_uv',torch.max(UV_grd).cpu().detach().numpy(),'min_uv',torch.min(UV_grd).cpu().detach().numpy())
             return UV_grd
 
+        # rescale UV to [0-1] grid
+        UV_grd[:,:,1,:,:] = UV_grd[:,:,1,:,:]/height
+        UV_grd[:,:,0,:,:] = UV_grd[:,:,0,:,:]/width
+
         # Lagrangian prediction
         Rgrd = input[:,-1,:,:,:] #use initial
 
@@ -106,7 +111,6 @@ class EF_el(nn.Module):
 
         xout = torch.zeros(bsize, tsize, channels, height, width,  requires_grad=True).cuda()
         if self.mode == "check":
-            uvout = torch.zeros(bsize, tsize, 2, height, width).cuda()
             r_pc_out = torch.zeros(bsize, tsize, channels, height*width).cuda()
             xy_pc_out = torch.zeros(bsize, tsize, 2, height*width).cuda()
         xzero = torch.zeros(bsize, channels, height, width,  requires_grad=True).cuda() # ! should I put zero here?
@@ -117,21 +121,23 @@ class EF_el(nn.Module):
             UV_pc = grid_to_pc(UV_grd[:,it,:,:,:],XY_pc)
             print('max_uv',torch.max(UV_pc).cpu().detach().numpy(),'min_uv',torch.min(UV_pc).cpu().detach().numpy())
             # Calc Time Progress
-            XY_pc = XY_pc + UV_pc*10.0
+            XY_pc = XY_pc + UV_pc
             XY_pc = torch.clamp(XY_pc,min=0.0,max=1.0) # XY should be in [0,1]
             # Interpolate PC to Grid
             Rgrd = pc_to_grid(R_pc,XY_pc,height)
 
             xout[:,it,:,:,:] = Rgrd
             if self.mode == "check":
-                uvout[:,it,:,:,:] = UV_grd
                 r_pc_out[:,it,:,:] = R_pc
                 xy_pc_out[:,it,:,:] = XY_pc
 
         if self.mode == "run":
             return xout
         elif self.mode == "check":
-            return xout,uvout,r_pc_out,xy_pc_out
+            # rescale back UV
+            UV_grd[:,:,1,:,:] = UV_grd[:,:,1,:,:]*height
+            UV_grd[:,:,0,:,:] = UV_grd[:,:,0,:,:]*width
+            return xout,UV_grd,r_pc_out,xy_pc_out
 
 class Predictor(nn.Module):
     def __init__(self, params):

src_post/plot_comp_prediction_artificial.py
@@ -52,6 +52,19 @@ def plot_vector_field(X,U,V,title,png_fpath,vmin=0,vmax=1):
     plt.savefig(png_fpath)
     plt.close()
 
+def plot_point_cloud(rain_pc,xy_pc,title,png_fpath,vmin=0,vmax=1):
+    plt.scatter(xy_pc[0,:], xy_pc[1,:], s=20, c=rain_pc, cmap="GnBu", edgecolors="grey")
+#    plt.xlim(0,1)
+#    plt.ylim(0,1)
+    plt.xlim(0.1,  0.2)
+    plt.ylim(0.2,  0.3)
+    plt.colorbar()
+    plt.grid()
+    plt.title(title)
+    plt.show()
+    plt.savefig(png_fpath)
+    plt.close()
+
 # plot comparison of predicted vs ground truth
 def plot_comp_prediction(data_path,filelist,model_fname,batch_size,tdim_use,
                          pic_path,scl,case,mode='png_whole'):
@@ -75,37 +88,51 @@ def plot_comp_prediction(data_path,filelist,model_fname,batch_size,tdim_use,
     #model.load_state_dict(torch.load(model_fname))
     # evaluation mode
     model.eval()
+    # activate check mode
+    model.mode = "check"
     #
     for i_batch, sample_batched in enumerate(valid_loader):
         fnames = sample_batched['fnames']
         # apply the trained model to the data
         input = Variable(scl.fwd(sample_batched['past'])).cuda()
         target = Variable(scl.fwd(sample_batched['future'])).cuda()
+        target_val = Variable(scl.fwd(sample_batched['future_val'])).cuda()
         #input = Variable(sample_batched['past']).cpu()
         #target = Variable(sample_batched['future']).cpu()
-        output = model(input)
-        
+        #output = model(input)
+        xout,uvout,r_pc_out,xy_pc_out = model(input)
+    
         for n,fname in enumerate(fnames):
             # convert to cpu
-            rain= input[n,:,0,:,:].cpu().data.numpy()
+            rain_tg= target_val[n,:,:,:,:].cpu().data.numpy()
             uv_tg = target[n,:,:,:,:].cpu().data.numpy()
-            uv_pred = output[n,:,:,:,:].cpu().data.numpy()
+            rain_pred= xout[n,:,:,:,:].cpu().data.numpy()
+            uv_pred = uvout[n,:,:,:,:].cpu().data.numpy()
+            # point cloud
+            rain_pc= r_pc_out[n,:,:,:].cpu().data.numpy()
+            xy_pc = xy_pc_out[n,:,:,:].cpu().data.numpy()
             
             for nt in range(12):
                 nt_str = '_dt%02d' % nt
                 png_fpath = pic_path+'comp_pred_'+str(n)+'_tg'+nt_str+'.png'
-                plot_vector_field(rain[nt,:,:],uv_tg[nt,0,:,:],uv_tg[nt,1,:,:],
-                                  'prediction:'+nt_str,png_fpath)
+                # plot ground truth
+                plot_vector_field(rain_tg[nt,0,:,:],uv_tg[nt,0,:,:],uv_tg[nt,1,:,:],
+                                  fname+' ground truth:'+nt_str,png_fpath)
+                # plot UV
                 png_fpath = pic_path+'comp_pred_'+str(n)+'_pred'+nt_str+'.png'
-                plot_vector_field(rain[nt,:,:],uv_pred[nt,0,:,:],uv_pred[nt,1,:,:],
-                                  'target:'+nt_str,png_fpath)
+                plot_vector_field(rain_pred[nt,0,:,:],uv_pred[nt,0,:,:],uv_pred[nt,1,:,:],
+                                  fname+' model prediction:'+nt_str,png_fpath)
+                # plot point cloud
+                png_fpath = pic_path+'comp_pred_'+str(n)+'_pc'+nt_str+'.png'
+                plot_point_cloud(rain_pc[nt,0,:],xy_pc[nt,:,:],
+                                  fname+' model prediction:'+nt_str,png_fpath)
         # free GPU memory (* This seems to be necessary if going without backprop)
-        del input,target,output
+        del input,target,xout,uvout,r_pc_out,xy_pc_out
         return
         
 if __name__ == '__main__':
     # params
-    batch_size = 4
+    batch_size = 10
     tdim_use = 12
 
     # read case name from command line