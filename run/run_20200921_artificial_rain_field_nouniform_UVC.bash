02197dd0630c77216e0ec7ea6c3c47b51813e84d
Tsuyoshi Inoue <inoue0406@gmail.com>
2020-09-21 18:32:56 +0900

    added nouniform artificial field generation

---
+++ run/run_20200921_artificial_rain_field_nouniform_UVC.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python ../src_prep/artificial_rain_field_nouniform_UVC.py 1 0 2000 >& log1 &
+python ../src_prep/artificial_rain_field_nouniform_UVC.py 2 2000 4000 >& log2 &
+python ../src_prep/artificial_rain_field_nouniform_UVC.py 3 4000 6000 >& log3 &
+python ../src_prep/artificial_rain_field_nouniform_UVC.py 4 6000 8000 >& log4 &
+python ../src_prep/artificial_rain_field_nouniform_UVC.py 5 8000 10000 >& log5 &
