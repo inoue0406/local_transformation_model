69ebaf7e8afaecac7a604d3bf5b0ac83d0a60d28
Tsuyoshi Inoue <inoue0406@gmail.com>
2021-01-03 12:46:58 +0900

    added multi-scale time average routine

--- src_prep/prep_radarJMA_averaged_alljapan.py
+++ src_prep/prep_radarJMA_averaged_alljapan.py
@@ -14,8 +14,45 @@ import os.path
 
 import datetime
 
+# pre-selected 
+slct_id = np.array([[2, 1],
+                    [2, 2],
+                    [3, 2],
+                    [3, 3],
+                    [4, 2],
+                    [4, 3],
+                    [4, 4],
+                    [4, 5],
+                    [4, 6],
+                    [4, 7],
+                    [5, 6],
+                    [5, 7],
+                    [5, 8],
+                    [5, 9],
+                    [6, 6],
+                    [6, 7],
+                    [6, 8],
+                    [6, 9],
+                    [7, 7],
+                    [7, 8],
+                    [7, 9],
+                    [7, 10],
+                    [8, 8],
+                    [8, 9],
+                    [8, 10],
+                    [8, 11],
+                    [8, 12],
+                    [8, 13],
+                    [9, 9 ],
+                    [9, 10],
+                    [9, 11],
+                    [9, 12],
+                    [9, 13],
+                    [10, 12],
+                    [10, 13]])
+
 # extract data from nc file
-def ext_nc_JMA(fname):
+def ext_nc_JMA_all(fname):
     try:
         nc = netCDF4.Dataset(fname, 'r')
     except:
@@ -34,8 +71,43 @@ def ext_nc_JMA(fname):
     lats = nc.variables['LAT'][:]
     R = nc.variables['PRATE'][:]
     #
-    R = R.T   # transpose so that i index comes first
-    return(R[:,:,0])
+    # clip area around tokyo
+    lat_tokyo = 35.681167
+    lon_tokyo = 139.767052
+    nx_clip = 200
+    ny_clip = 200
+    i0=np.argmin(np.abs(lons.data-lon_tokyo)) - int(nx_clip/2)
+    j0=np.argmin(np.abs(lats.data-lat_tokyo)) - int(ny_clip/2)
+    i1=i0+nx_clip
+    j1=j0+ny_clip
+    # check
+    if((i0 != 1641) or(j0 != 1781)):
+        print("irregular i0,j0: skip.. ",i0,j0)
+        return None
+
+    Rclip_list = []
+    for n in range(slct_id.shape[0]):
+        ii,jj = slct_id[n,:]
+        # choose ii and jj to span the whole dataset area
+        Rslct = np.zeros(35)
+        count = 0
+        # offset
+        ii = ii - 8
+        jj = jj - 8
+        #
+        ii0 = i0 + nx_clip*ii
+        jj0 = j0 + ny_clip*jj
+        ii1 = ii0 + nx_clip
+        jj1 = jj0 + ny_clip
+        # extract data
+        Rclip = R.data[0,jj0:jj1,ii0:ii1]
+        Rclip = Rclip.T   # transpose so that i index comes first
+        lon_clip=lons.data[i0:i1]
+        lat_clip=lats.data[j0:j1]
+        Rclip_list.append(Rclip)
+    # stack in 0 dimension
+    Rclip_grd = np.stack(Rclip_list,axis=0)
+    return(Rclip_grd)
 
 def fname_1h_ago(fname):
     f2 = fname.split("/")[-1]
@@ -48,9 +120,49 @@ def fname_1h_ago(fname):
     fname1 = date1.strftime('/data/nas_data/jma_radar/%Y/2p-jmaradar5_%Y-%m-%d_%H%Mutc.nc.gz')
     return fname1
 
+def prep_1h_grid(year,infile_root,outfile_root):
+    print("preparing for year= ",year)
+    
+    for infile in sorted(glob.iglob(infile_root + '*/*/*00utc.nc.gz')):
+        # read 1hour data at a time
+        # initialize with -999.0
+        R_list = []
+        for i in range(12):
+            shour = '{0:02d}utc'.format(i*5)
+            in_zfile = infile.replace('00utc',shour)
+            print('reading zipped file:',in_zfile)
+            # '-k' option for avoiding removing gz file
+            subprocess.run('gunzip -kf '+in_zfile,shell=True)
+            in_nc=in_zfile.replace('.gz','')
+            print('reading nc file:',in_nc)
+            if os.path.exists(in_nc):
+                R_list.append(ext_nc_JMA_all(in_nc))
+                if R_list is None:
+                    next
+            else:
+                print('nc file not found!!!',in_nc)
+                next
+            subprocess.run('rm '+in_nc,shell=True)
+        # create 1h data
+        print("number of data in an hour",len(R_list))
+        Rhour = np.stack(R_list,axis=0)
+        R1h = np.mean(Rhour,axis=0)
+        R1h = R1h.astype("float16")
+        
+        for n in range(slct_id.shape[0]):
+            # save 1h data as h5 file
+            ii,jj = slct_id[n,:]
+            h5fname = infile.split("/")[-1]
+            h5fname = "1havg_"+h5fname.replace('.nc.gz','_'+str(ii)+'_'+str(jj)+'.h5')
+            print('writing h5 file:',h5fname)
+            h5file = h5py.File(outfile_root+h5fname,'w')
+            h5file.create_dataset("R",data= R1h[n,:,:])
+            h5file.close() 
+
 if __name__ == '__main__':
     #for year in [2015,2016,2017]:
-    year = 2015
+    #year = 2015
+    #year = 2016
     #year = 2010
     #year = 2011
     #year = 2012
@@ -67,38 +179,6 @@ if __name__ == '__main__':
     outfile_root = '../data/data_alljapan_averaged/'
     print('output dir:',outfile_root)
 
-    for infile in sorted(glob.iglob(infile_root + '*/*/*00utc.nc.gz')):
-        # read 1hour data at a time
-        # initialize with -999.0
-        R_list = []
-        for i in range(12):
-            shour = '{0:02d}utc'.format(i*5)
-            in_zfile = infile.replace('00utc',shour)
-            print('reading zipped file:',in_zfile)
-            # '-k' option for avoiding removing gz file
-            subprocess.run('gunzip -kf '+in_zfile,shell=True)
-            in_nc=in_zfile.replace('.gz','')
-            print('reading nc file:',in_nc)
-            if os.path.exists(in_nc):
-                R_list.append(ext_nc_JMA(in_nc))
-                if R_list is None:
-                    next
-            else:
-                print('nc file not found!!!',in_nc)
-                next
-            subprocess.run('rm '+in_nc,shell=True)
-        # create 1h data
-        print("number of data in an hour",len(R_list))
-        Rhour = np.stack(R_list,axis=0)
-        R1h = np.mean(Rhour,axis=0)
-        # create masked array for saving space
-        R1hm = np.ma.masked_array(R1h,mask=R1h<-30000.0)
-        # save 1h data as h5 file
-        outfile = infile.split("/")[-1]
-        outfile = "1havg_" + outfile.replace("nc.gz","h5")
-        print('writing h5 file:',outfile)
-        h5file = h5py.File(outfile_root+outfile,'w')
-        #h5file.create_dataset('R',data= R1hm)
-        h5file.create_dataset('R',data= R1hm,compression="gzip")
-        h5file.close() 
+    prep_1h_grid(year,infile_root,outfile_root)
+
 
---
+++ src_prep/prep_radarJMA_multiscale-avg_alljapan.py
@@ -0,0 +1,144 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import h5py
+
+import glob
+import gzip
+import subprocess
+import sys
+import os.path
+
+import datetime
+
+# pre-selected 
+slct_id = np.array([[2, 1],
+                    [2, 2],
+                    [3, 2],
+                    [3, 3],
+                    [4, 2],
+                    [4, 3],
+                    [4, 4],
+                    [4, 5],
+                    [4, 6],
+                    [4, 7],
+                    [5, 6],
+                    [5, 7],
+                    [5, 8],
+                    [5, 9],
+                    [6, 6],
+                    [6, 7],
+                    [6, 8],
+                    [6, 9],
+                    [7, 7],
+                    [7, 8],
+                    [7, 9],
+                    [7, 10],
+                    [8, 8],
+                    [8, 9],
+                    [8, 10],
+                    [8, 11],
+                    [8, 12],
+                    [8, 13],
+                    [9, 9 ],
+                    [9, 10],
+                    [9, 11],
+                    [9, 12],
+                    [9, 13],
+                    [10, 12],
+                    [10, 13]])
+
+def load_avg_field(infile_root,ii,jj):
+    print("load averaged fields")
+
+    ijstr = "_%d_%d.h5" % (ii,jj)
+    rain_X_list = []
+    files_list = sorted(glob.iglob(infile_root + "*" + ijstr))
+    for infile in files_list:
+        print("reading:",infile)
+        h5file = h5py.File(infile,'r')
+        rain_X = h5file['R'][()].astype(np.float16)
+        rain_X = np.maximum(rain_X,0) # replace negative value with 0
+        rain_X_list.append(rain_X)
+    # stack in 0 dimension
+    rain_X_all = np.stack(rain_X_list,axis=0)
+    return rain_X_all,files_list
+
+def multi_scale_avg(Vavg,flist,fname):
+    flist = [fname.split("/")[-1] for fname in flist]
+    # averaging at multiple time-scale
+    f2 = fname.split("/")[-1]
+    ijstr = "utc_%d_%d.h5" % (ii,jj)
+    f2 = f2.replace("2p-jmaradar5_","").replace(ijstr,"")
+    basedate = datetime.datetime.strptime(f2,'%Y-%m-%d_%H%M')
+    
+    Nexp = 10 # number of exponents 2^Nexp
+    startdate = datetime.datetime(2015, 1, 1)
+    mindate = basedate - datetime.timedelta(hours=2**(Nexp-1))
+    if mindate < startdate:
+        print("skipped: there is no record for averaging",basedate)
+        return None
+    Vmsa_list = []
+    for Nexp in range(Nexp):
+        time_avg = range(1,2**Nexp)
+        print("time averaging with hours:",time_avg)
+        date_list = [basedate - datetime.timedelta(hours=x) for x in time_avg]
+        favg_list = [date.strftime('1havg_2p-jmaradar5_%Y-%m-%d_%H%M'+ijstr) for date in date_list]
+        #
+        #df_fname = pd.DataFrame(,columns=["fname"])
+        intersect = list(set(favg_list) & set(flist))
+        id_list = []
+        for x in intersect:
+            id_list.append(flist.index(x))
+        Vaa = Vavg[id_list,:,:]
+        Vaa = np.mean(Vaa,axis=0)
+        Vmsa_list.append(Vaa)
+        
+    # whole-time averaging
+    Vmsa_list.append(np.mean(Vavg,axis=0))
+    # stack in 0 dimension
+    Vmsa = np.stack(Vmsa_list,axis=0)
+    return Vmsa
+
+def prep_multi_average(infile_root,outfile_root,anno_list,ii,jj):
+    print("preparing for the region= ",ii,jj)
+
+    # read averaged field
+    Vavg,flist = load_avg_field(infile_root,ii,jj)
+
+    # loop through data
+    ijstr = "_%d_%d.h5" % (ii,jj)
+    anno_list_ij = anno_list.loc[anno_list["fname"].str.contains(ijstr)]
+    
+    for index, row in anno_list_ij.iterrows():
+        print
+        Vmsa = multi_scale_avg(Vavg,flist,row["fname"])
+        if Vmsa is None:
+            continue
+        h5fname = row["fname"]
+        print('writing h5 file:',h5fname)
+        h5file = h5py.File(outfile_root+h5fname,'w')
+        h5file.create_dataset("Ravg",data= Vmsa)
+        h5file.close()
+    
+if __name__ == '__main__':
+    year = 2015
+    #year = 2016
+
+    # read
+    infile_root = "../data/data_alljapan_averaged/"
+    print('input dir:',infile_root)
+
+    # outfile
+    outfile_root = '../data/data_alljapan_multiavg/'
+    print('output dir:',outfile_root)
+
+    # list of data
+    anno_path = "../data/train_alljapan_2yrs_JMARadar.csv"
+    anno_list = pd.read_csv(anno_path)
+
+    # prepare grid by grid
+    for n in range(slct_id.shape[0]):
+        ii,jj = slct_id[n,:]
+        prep_multi_average(infile_root,outfile_root,anno_list,ii,jj)
