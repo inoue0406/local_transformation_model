ac351acb322d7b9a621f57b626965b75be658e72
Tsuyoshi Inoue <inoue0406@gmail.com>
2021-01-09 15:44:26 +0900

    new case

---
+++ run/run_20201220_trajGRU_size200_mse_8years_tst.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+case="result_20201216_trajGRU_size200_8years_mse_lr0002_NIED"
+
+#       --train_path ../data/JMA_9years_combined_train.csv \
+#       --train_path ../data/train_alljapan_2014_JMARadar.csv \
+
+# running script for Rainfall Prediction with ConvLSTM
+python ../src/main_trajGRU_jma.py --model_name trajgru --no_train\
+       --dataset radarJMA --model_mode run --data_scaling linear\
+       --aug_rotate 0 --aug_resize 0\
+       --data_path ../data/ --image_size 200\
+       --valid_data_path ../data/data_kanto/ \
+       --train_path ../data/JMA_8years_combined_train.csv \
+       --valid_path ../data/valid_simple_JMARadar.csv\
+       --test --eval_threshold 0.5 10 20 --test_path ../data/valid_simple_JMARadar.csv\
+       --result_path $case --tdim_use 12 --tdim_loss 12 --learning_rate 0.0002 --lr_decay 0.9\
+       --batch_size 10 --n_epochs 20 --n_threads 4 --checkpoint 5 \
+       --loss_function MSE\
+       --hidden_channels 5 --kernel_size 3 --optimizer adam \
+# post plot
+python ../src_post/plot_comp_prediction_trajgru.py $case
+python ../src_post/gif_animation.py $case
+
