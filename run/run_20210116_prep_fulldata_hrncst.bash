b3d5bc6564dd19553176e21d159b16bb5d9a879f
Tsuyoshi Inoue <inoue0406@gmail.com>
2021-01-30 01:13:43 +0900

    add case

--- run/run_20210116_prep_fulldata_hrncst.bash
+++ run/run_20210116_prep_fulldata_hrncst.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-python ../src_prep/prep_hrncst_JMA_alljapan.py 2017-01 >& log-hr-01 &
+#python ../src_prep/prep_hrncst_JMA_alljapan.py 2017-01 >& log-hr-01 &
 python ../src_prep/prep_hrncst_JMA_alljapan.py 2017-02 >& log-hr-02 &
 #python ../src_prep/prep_hrncst_JMA_alljapan.py 2017-03 >& log-hr-03 &
 #python ../src_prep/prep_hrncst_JMA_alljapan.py 2017-04 >& log-hr-04 &
